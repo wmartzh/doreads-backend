3e3739292125d69e855d7aec9291b2041e34fc19
Andrei Rivera <andreirive2003@gmail.com>
2023-02-17 16:27:23 -0600

    Student Router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,12 @@
 import { Application } from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
 import authRouter from "./auth";
+import studentRouter from "./student";
 
 export default function router(app: Application): void {
   /**
    * Every source are specifed here
    */
   app.use("/auth", authRouter);
+  app.use("/student", authMiddleware, studentRouter);
 }
