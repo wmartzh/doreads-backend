a3e3e944bd9b7abda12bf286e419a0646ca21724
Andrei Rivera <andreirive2003@gmail.com>
2023-02-21 22:26:38 -0600

    Fix to work with the updated schema

--- src/models/student.models.ts
+++ src/models/student.models.ts
@@ -1,12 +1,12 @@
 import * as Joi from "joi";
 
 export const RegisterStudentSchema = Joi.object({
-  code: Joi.number().required(),
+  code: Joi.string().required(),
   name: Joi.string().required(),
   email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
     .required(),
-  phone: Joi.number().required(),
+  phone: Joi.string().required(),
 });
 
 export const ChangeStudentStatusSchema = Joi.object({
@@ -15,10 +15,10 @@ export const ChangeStudentStatusSchema = Joi.object({
 });
 
 export const UpdateStudentSchema = Joi.object({
-  code: Joi.number(),
+  code: Joi.string(),
   name: Joi.string(),
   email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } }),
   status: Joi.string().valid("ACTIVE", "BLOCKED", "INACTIVE"),
-  phone: Joi.number(),
+  phone: Joi.string(),
 }).min(1);
