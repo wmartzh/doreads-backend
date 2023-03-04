44085e0b6055f2d7002468d23f73ad4f0faa6294
Andrei Rivera <andreirive2003@gmail.com>
2023-03-03 19:53:25 -0600

    Default port change to 8000
    Port 8001 was previous but swagger was sending 8000 so to make them the same I changed this one

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import Server from "./src/server";
 import router from "./src/routes/router";
 
-const PORT = parseInt(process.env.PORT || "8001");
+const PORT = parseInt(process.env.PORT || "8000");
 const HOSTNAME = process.env.HOST || "localhost";
 export default new Server().router(router).listen(PORT, HOSTNAME);
